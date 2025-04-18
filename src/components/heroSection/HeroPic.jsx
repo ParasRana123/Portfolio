5d4b3b8409ec16a6b775474c226222d57df2727a
ParasRana123 <parasrana579@gmail.com>
2025-04-19 03:42:04 +0530

    Fixed the image file paths for deployment

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -11,7 +11,7 @@ const HeroPic = () => {
     whileInView='show'
     viewport={{once: false , amount: 0}}
     className='h-full flex items-center justify-center'>
-        <img src="../../public/images/paras.png" alt="Paras Rana" className='max-h-[450px] w-auto' />
+        <img src="/images/paras.png" alt="Paras Rana" className='max-h-[450px] w-auto' />
         <div className='absolute -z-10 flex justify-center items-center animate-pulse'>
         <FiHexagon className='md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan-400 blur-md animate-[spin_20s_linear_infinite]'/>
         </div>
--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -4,7 +4,7 @@ const SubSkills = () => {
   return (
     <div className='border-y-2 border-gray-600 relative'>
         <div className='absolute bg-gradient-to-r from-orange-500 to-cyan-600 opacity-50 w-full h-full'></div>
-        <img src="../../public/images/subSkills.jpg" alt="Sub Skills" />
+        <img src="/images/subSkills.jpg" alt="Sub Skills" />
     </div>
   )
 }
