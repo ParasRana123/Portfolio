482c61c41a191f0e1d3b288eba98a7a0f5334afa
ParasRana123 <parasrana579@gmail.com>
2025-04-19 04:53:16 +0530

    Changed into a fully responive hero section and skills section

--- src/components/aboutMeSection/AboutMeMain.jsx
+++ src/components/aboutMeSection/AboutMeMain.jsx
@@ -7,7 +7,7 @@ import { fadeIn } from '../../framerMotion/variants'
 const AboutMeMain = () => {
   return (
     <div 
-    id='about' className='flex md:flex-row sm:flex-col gap-12 px-4 max-w-[1200px] mx-auto mt-[100px] justify-between items-center'>
+    id='about' className='flex flex-col md:flex-row sm:flex-col gap-12 px-4 max-w-[1200px] mx-auto mt-[100px] justify-between items-center'>
         <motion.div
         variants={fadeIn("right" , 0.2)}
         initial='hidden'
