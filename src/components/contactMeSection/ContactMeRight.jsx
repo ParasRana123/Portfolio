c22dd6683ee1d496a8ae7c3689fcb68a673394c4
ParasRana123 <parasrana579@gmail.com>
2025-04-19 18:17:43 +0530

    Fixed the visiblity of Social icons and the email image below the contact form

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -4,7 +4,7 @@ import ContactSocial from './ContactSocial'
 
 const ContactMeRight = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-12'>
+    <div className='flex flex-col items-center justify-center gap-12 w-full'>
       <img src="/images/email-image.png" alt="" className='max-w-[300px]' />
       <ContactInfo />
       <ContactSocial />
--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,7 +6,7 @@ import { FaInstagram } from "react-icons/fa";
 
 const ContactSocial = () => {
   return (
-    <div className='flex gap-4'>
+    <div className='flex gap-4 flex-wrap justify-center'>
         <SingleContactSocial Link='https://www.linkedin.com/in/paras-rana-696b7731b/' Icon={FaLinkedin}/>
         <SingleContactSocial Link='https://github.com/ParasRana123' Icon={FaGithub}/>
         <SingleContactSocial Link='https://www.instagram.com/_theparas_/' Icon={FaInstagram}/>
