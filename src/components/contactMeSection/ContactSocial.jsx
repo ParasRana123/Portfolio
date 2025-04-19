88e665cee9b4c3eef66d53fb1503e0385b63441c
ParasRana123 <parasrana579@gmail.com>
2025-04-19 19:45:02 +0530

    Replaced Insta icon with Leetcode

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import SingleContactSocial from './SingleContactSocial'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 const ContactSocial = () => {
   return (
     <div className='flex gap-4 flex-wrap justify-center'>
         <SingleContactSocial Link='https://www.linkedin.com/in/paras-rana-696b7731b/' Icon={FaLinkedin}/>
         <SingleContactSocial Link='https://github.com/ParasRana123' Icon={FaGithub}/>
-        <SingleContactSocial Link='https://www.instagram.com/_theparas_/' Icon={FaInstagram}/>
+        <SingleContactSocial Link='https://leetcode.com/u/lrmjJGbWG0/' Icon={SiLeetcode}/>
     </div>
   )
 }
