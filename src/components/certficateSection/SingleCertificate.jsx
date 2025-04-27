cef8f97e47181c1080341e261569d6cd7715e7ac
ParasRana123 <parasrana579@gmail.com>
2025-04-27 18:05:40 +0530

    Further enhanced the styling

--- src/components/certficateSection/SingleCertificate.jsx
+++ src/components/certficateSection/SingleCertificate.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const SingleCertificate = ({ name, year, issued, link }) => {
   return (
-    <div className="border border-gray-300 rounded-2xl p-6 shadow-xl hover:shadow-2xl transition duration-300 bg-white dark:bg-gray-800 w-full h-full flex flex-col justify-between">
+    <div className="border border-gray-300 rounded-2xl p-6 bg-[#61453d] dark:bg-gray-800 w-full h-full flex flex-col justify-between shadow-[0_4px_20px_rgba(255,255,255,0.3)] hover:shadow-[0_6px_25px_rgba(255,255,255,0.5)] hover:scale-105 transition transform duration-300">
       <div>
-        <h2 className="text-lg sm:text-xl font-semibold mb-4 text-gray-800 dark:text-white">{name}</h2>
-        <p className="text-gray-600 dark:text-gray-300 mb-2"><span className="font-semibold">Year:</span> {year}</p>
-        <p className="text-gray-600 dark:text-gray-300 mb-4"><span className="font-semibold">Issued by:</span> {issued}</p>
+        <h2 className="text-lg sm:text-xl font-semibold mb-4 text-white">{name}</h2>
+        <p className="text-gray-400 dark:text-gray-300 mb-2"><span className="font-semibold">Year:</span> {year}</p>
+        <p className="text-gray-400 dark:text-gray-300 mb-4"><span className="font-semibold">Issued by:</span> {issued}</p>
       </div>
       <a 
         href={link} 
